95234ad3975644b8a46dbf1236059d85b7b8334f
Devin Zhang <qq1392137041@gmail.com>
2011-08-19 19:25:43 +0800

    group album framework,need more details

---
+++ sites/all/modules/customized/og_album/js/og_album.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+   $('a#add-profile-picture').click(function(){
+   /*
+    $('#add-profile-avator').dialog({ 
+        position: 'center',
+        maxHeight: 300,
+        });
+        
+        
+    return false;
+    */
+    $('a#add-profile-picture').click(function(){
+    $('#add-profile-avator').show();
+    });
+   });
+});
+
