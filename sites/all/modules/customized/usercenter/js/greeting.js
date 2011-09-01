78edb0b3617663dd0b234a97641ffd6e001244ea
devin <1392137041@qq.com>
2011-09-01 16:46:11 +0800

    add jquery dialog

--- sites/all/modules/customized/usercenter/js/greeting.js
+++ sites/all/modules/customized/usercenter/js/greeting.js
@@ -5,17 +5,32 @@ $(document).ready(function() {
    $('a.delete-greeting-link').click(function(){
      var id = $(this).attr('id');
      var li= '#greeting-entry-' + id;
+     var link = $(this).attr('href');
      $(li).css('background-color','#fee');
      $("#dialog").dialog({
       buttons : {
-        "Confirm" : function() {          
-           $(this).dialog("close");
-          $(li).hide('slow').empty();
+        "Confirm" : function() {                     
+          
+           $.ajax({
+	            url: link,
+	            type: "GET", 
+	            dataType: "json",	  
+                async: true,                
+	            timeout: 9000, 
+	            error: function(xhr, status, error){	                
+	                    },
+	            success: function(data){   
+	               $(li).hide('slow').empty();
+	            }                
+			}); 
         },
         "Cancel" : function() {
           $(this).dialog("close");
           $(li).css('background-color','#fff');
         }
+      },
+      beforeClose: function() {
+         $(li).css('background-color','#fff');
       }
     });
      $("#dialog").dialog("open");
