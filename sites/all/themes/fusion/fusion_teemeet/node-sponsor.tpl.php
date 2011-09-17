7ab08d76e98b05d637d955a4b444cef8d1bc1f8c
devin <1392137041@qq.com>
2011-09-18 00:07:13 +0800

    adding sponsor processes, the framework is ok

--- sites/all/themes/fusion/fusion_teemeet/node-sponsor.tpl.php
+++ sites/all/themes/fusion/fusion_teemeet/node-sponsor.tpl.php
@@ -10,7 +10,7 @@
                 <abbr title="Maryland" class="region"><?php echo $node->field_sponsor_address[0]['province'];?></abbr>,
                 <span class="country-name"><?php echo $node->field_sponsor_address[0]['street'];?></span>
                 </span>
-                <span id="sponsoringSince">Sponsoring Meetup Groups since <?php echo format_date($node->created,'custom','M d, Y'); ?></span>
+                <span id="sponsoringSince"><?php echo t('Sponsoring Meetup Groups since');?> <?php echo format_date($node->created,'custom','M d, Y'); ?></span>
             </div>
 
             <ul class="D_actions editControls">
@@ -19,18 +19,18 @@
         </div>
         <div class="D_boxsection isNotDivided ">
             <div class="clearfix" id="offerStatus">
-                <h2 class="lightHeading" id="banner"><span>Our Perk for Meetup Groups…</span></h2>
+                <h2 class="lightHeading" id="banner"><span><?php echo t('Our Perk for Meetup Groups');?>…</span></h2>
             </div>
             <div class="clearfix noPerk" id="featuredOffer">
                 <div class="offerSections">
                     <div class="offerSection clearfix">
-                        <h3 class="lightHeading"><span>You're not currently offering a Perk</span></h3>
-                        <p style="line-height: 1.3;">Find new customers, promote your brand, or just support local community. Offer a Perk to any of 90,000+ Meetup Groups!</p>
+                        <h3 class="lightHeading"><span><?php echo t('You\'re not currently offering a Perk to any group'); ?></span></h3>
+                        <p style="line-height: 1.3;"><?php echo t('Find new customers, promote your brand, or just support local community. Offer a Perk to any of 90,000+ Teemeet Groups!'); ?></p>
                         <div id="createPerk">
-                            <a class="D_submit" href="<?php echo url('perk/create'); ?>">
-                            Create a Perk
+                            <a class="D_submit" href="<?php echo url('sponsor/findgroup'); ?>">
+                            <?php echo t('Find a group to sponsor'); ?>
                             </a>
-                            <a class="alternate" href="#">Learn more</a>
+                            <a class="alternate" href="#"><?php echo t('Learn more'); ?></a>
                         </div>
                     </div>
                 </div>
