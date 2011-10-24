3f3b9292ab265acdae114493971ec80e7e6ddd6f
wannianchuan <wannianchuan@gmail.com>
2011-10-24 16:13:39 +0800

    Translate '/sponsor/create'.

--- sites/all/modules/customized/sponsor/sponsor-sponsorship-landing.tpl.php
+++ sites/all/modules/customized/sponsor/sponsor-sponsorship-landing.tpl.php
@@ -9,66 +9,66 @@
 	
 <div id="blurb">
 
-<p>Find new customers, promote your brand, or just support local community. Offer a Perk to any of 90,000+ Meetup Groups! <a href="http://www.meetup.com/help/What-is-Meetup-Perks" _target="blank">Learn more...</a></p>
+<p>发现新客户，提升你的品牌或者仅仅是支持下你们本地的社团.提供一个福利给你们的小组! </p>
 </div>
 <div class="clearfix" id="groups">
 <div class="group">
 <div id="group1" style="background: url(&quot;<?php global $base_url; echo $base_url.'/'.drupal_get_path('module','usercenter');?>/images/pupsAndUpPlayGroup.jpg&quot;) no-repeat scroll center center rgb(102, 102, 102);" class="photo">
 </div>
 <div class="main">
-<a href="http://www.meetup.com/sponsor/From-the-Heart-Doggie-Daycare/">From the Heart Doggie Daycare</a>
+<a href="">来自青岛市的登山户外商品店</a>
 </div>
 <div class="followup">
-is giving <strong>15% off any service</strong>
-to members of the <a target="new" href="http://www.meetup.com/Pups-and-Up-Play-Group/">Pups &amp; Up Play Group</a>
+正在提供 <strong>15%的打折折扣</strong>
+给会员 <a target="new" href="">青岛登山小组</a>
 </div>
 </div>
 <div class="group">
 <div id="group2" style="background: url(&quot;<?php global $base_url; echo $base_url.'/'.drupal_get_path('module','usercenter');?>/images/southFloridaScubaMUG.jpg&quot;) no-repeat scroll center center rgb(102, 102, 102);" class="photo">
 </div>
 <div class="main">
-<a href="http://www.meetup.com/sponsor/Lauderdale-Diver">Lauderdale Diver</a>
+<a href="">北京足球俱乐部</a>
 </div>
 <div class="followup">
-is giving <strong>10% off all Freediving and Spearfishing Gear</strong>
-to members of <a target="new" href="http://www.meetup.com/ScubaSoFL/">The South Florida Scuba Meetup Group</a>
+正在提供<strong>10%折扣和免费饮料</strong>
+给会员 <a target="new" href="">来自北京海淀回龙观小区足球爱好者小组</a>
 </div>
 </div>
 <div class="group">
 <div id="group3" style="background: url(&quot;<?php global $base_url; echo $base_url.'/'.drupal_get_path('module','usercenter');?>/images/nycOutdoorFitness.jpg&quot;) no-repeat scroll center center rgb(102, 102, 102);" class="photo">
 </div>
 <div class="main">
-<a href="">LifeLabs New York</a>
+<a href="">时尚你我公司</a>
 </div>
 <div class="followup">
-is giving a <strong>15% discount on courses &amp; adventures</strong>
-to members of <a target="new" href="http://www.meetup.com/OutdoorFitness/">NYC Outdoor Fitness</a>
+正在提供<strong>100%现金支持</strong>
+给<a target="new" href="">上海爱美女士俱乐部小组</a>
 </div>
 </div>
 <div class="group">
 <div id="group4" style="background: url(&quot;<?php global $base_url; echo $base_url.'/'.drupal_get_path('module','usercenter');?>/images/PortlandMahJongMUG.jpg&quot;) no-repeat scroll center center rgb(102, 102, 102);" class="photo">
 </div>
 <div class="main">
-<a href="http://www.meetup.com/sponsor/Quan-Linh-Asian-Bistro/">Quan Linh Asian Bistro</a>
+<a href="">深圳iphone开发协会</a>
 </div>
 <div class="followup">
-is giving a <strong>10% discount off the bill</strong>
-to members of the <a target="new" href="http://www.meetup.com/pdx-mahjong/">Portland MahJong Society</a>
+提供<strong>测试用itouch</strong>
+给会员<a target="new" href="">深圳iOS爱好者</a>
 </div>
 </div>
 </div>
 <div class="clearfix" id="chooseYourOwnAdventure">
 <div id="fullserv">
-<h3>Thinking a little bigger?</h3>
-<p>Starting at $10,000 a month, our team will work with your business or brand to create a full-service Meetup Sponsorship program.</p>
-<a class="trampoline" href="http://www.meetup.com/sponsorships/contact_meetuphq/">Find out more →</a>
+<h3>想做的更大点么?</h3>
+<p>想获取每月10000RMB的收入？想获得更大推广？那就和你和你的组员们来加入吧.</p>
+<a class="trampoline" href="">查找更多 →</a>
 </div>
 <div id="selfserv">
-<h3>Offer a Perk to Meetup Groups!</h3>
+<h3>提供一个福利给小组!</h3>
 <ol>
-<li>Come up with a compelling discount or member Perk.</li>
-<li>We'll review, approve, and share your Perk with relevant Meetup Groups.</li>
-<li>You pay $5 per month per Meetup Group that accepts your Perk.<br><strong>(Free until further notice!)</strong></li>
+<li>提供一个竞争力的折扣或者会员福利.</li>
+<li>我们会来评估，批准并提供给相关的小组.</li>
+<li>免费.<br><strong></strong></li>
 </ol>
 
 <div class="trampoline">
